66a92d396b741a61c0ab8db1c83dcf5890620fae
Ron Raifer <ronraifer@gmail.com>
2019-12-25 19:32:39 +0200

    a

---
+++ Project/G11_Server/src/common/DBHandler.java
@@ -0,0 +1,31 @@
+package common;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBHandler {
+	private Connection conn;
+
+	public Connection databaseConnect()
+	{
+		try 
+		{
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+        } catch (Exception ex) {/* handle the error*/}
+        
+        try 
+        {
+        	conn = DriverManager.getConnection("jdbc:mysql://remotemysql.com/q3t6Vm6bTC?autoReconnect=true&useSSL=false","q3t6Vm6bTC","hLwAfZ78Fx");
+        	//For Local:
+            //conn = DriverManager.getConnection("jdbc:mysql://localhost/OBLdb?autoReconnect=true&useSSL=false","root","Aa123456");
+            System.out.println("SQL connection succeed");
+     	} catch (SQLException ex) 
+     	    {	/* handle any errors*/
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+            } 
+        return conn;
+   	}
+}
