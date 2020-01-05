96a25869b71a25217e4a4b8248ad031f9d5f101c
aviran <aviran217@gmail.com>
2020-01-05 17:52:28 +0200

    started working on myrequest controller
    need to set request_handling table primary key (uneditable currently)

--- G11_ICM/Client/src/common/MsgEnum.java
+++ G11_ICM/Client/src/common/MsgEnum.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 public enum MsgEnum implements Serializable {
 	LOGIN, LOGIN_ERROR,
-	LOGOUT, ADD_REQUEST,ADD_FILES,SEND_ID_OF_REQUEST_TO_CLIENT;
+	LOGOUT, ADD_REQUEST,ADD_FILES,SEND_ID_OF_REQUEST_TO_CLIENT,
+	GET_REQUESTS_BY_ID;
 	
 }
