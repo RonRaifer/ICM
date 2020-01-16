25639d603bf19f23a3545f127f0e4b39293dd9a3
segev <segev@DESKTOP-TJK9K3V>
2020-01-16 19:54:31 +0200

    changes in activityreport,reportpane and reportscontroller

--- G11_ICM/Client/src/controller/ReportsController.java
+++ G11_ICM/Client/src/controller/ReportsController.java
@@ -1,10 +1,13 @@
 package controller;
 
+import entity.ActivityReport;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
 
 public class ReportsController {
@@ -22,6 +25,14 @@ public class ReportsController {
 
     @FXML
     private AnchorPane Duration;
+    @FXML
+    private TableView<ActivityReport> tblActivityReports;
+
+    @FXML
+    private TableColumn<ActivityReport, String> colStartDate;
+
+    @FXML
+    private TableColumn<ActivityReport, String> colEndDate;
 
     @FXML
     void generateNewReport(ActionEvent event) {
