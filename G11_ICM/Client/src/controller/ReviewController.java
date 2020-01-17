a6003fe44581c863933d9e5ec0084d457e730fce
aviran <aviran217@gmail.com>
2020-01-17 14:14:53 +0200

    changes in processes controller

--- G11_ICM/Client/src/controller/ReviewController.java
+++ G11_ICM/Client/src/controller/ReviewController.java
@@ -82,7 +82,7 @@ public class ReviewController implements Initializable {
 				e.printStackTrace();
 			}
 		 
-		 Alert alert = new Alert(AlertType.INFORMATION);
+		 	Alert alert = new Alert(AlertType.INFORMATION);
 	    	alert.setTitle("Information Dialog");
 	    	alert.setHeaderText(null);
 	    	alert.setContentText("Your decision has been submitted");
