1c001c491eeee5a6515d8989823283a9bf156835
segev <segev@DESKTOP-TJK9K3V>
2020-01-18 18:42:14 +0200

    changes in reports

---
+++ G11_ICM/Client/src/controller/PerformanceReportPopupController.java
@@ -0,0 +1,11 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class PerformanceReportPopupController {
+	@FXML
+    private Label durationApprovedExtensions;
+	@FXML
+	private Label durationActivityTimeAdded;
+}
